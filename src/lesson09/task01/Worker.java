0324d973381ab93d8f5646b3a1e026509c5ad657
MangutovaAlina <avmangutova.gmail.com>
2020-01-15 17:15:32 +0700

    Работа с БД

--- src/lesson09/task01/Worker.java
+++
@@ -1,9 +0,0 @@
-package lesson09.task01;
-
-/**
- * класс Worker
- */
-public interface Worker {
-    void doWork();
-
-}
--- src/lesson12/task02/Person.java
+++
@@ -1,5 +0,0 @@
-package lesson12.task02;
-
-public interface Person {
-    void personHello(String name);
-}
