06eed0c4fa0a579e8a4668107a2f63dc55365a5b
MangutovaAlina <avmangutova.gmail.com>
2020-01-15 20:11:07 +0700

    SpyMoscito тестирование

--- src/lesson15/Main.java
+++ src/lesson15/Main.java
@@ -3,7 +3,6 @@ package lesson15;
 import lesson15.connectionManager.ConnectionManager;
 import lesson15.connectionManager.ConnectionManagerJdbcImpl;
 import lesson15.dao.RoleDAO;
-import lesson15.dao.TableDAO;
 import lesson15.dao.UserDAO;
 import lesson15.dao.UserRoleDAO;
 import lesson15.pojo.Role;
@@ -18,23 +17,8 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * Задание:
- * 1) Спроектировать базу
- * Таблица USER содержит поля id, name, birthday, login_ID, city, email, description
- * Таблица ROLE содержит поля id, name (принимает значения Administration, Clients, Billing), description
- * Таблица USER_ROLE содержит поля id, user_id, role_id
- * Типы полей на ваше усмотрению, возможно использование VARCHAR(255)
- * <p>
- * 2) Через jdbc интерфейс сделать запись данных(INSERT) в таблицу
- * a) Используя параметризированный запрос
- * b) Используя batch процесс
- * 3) Сделать параметризированную выборку по login_ID и name одновременно
- * 4) Перевести connection в ручное управление транзакциями
- * a) Выполнить 2-3 SQL операции на ваше усмотрение (например, Insert в 3 таблицы – USER, ROLE, USER_ROLE)
- * между sql операциями установить логическую точку сохранения(SAVEPOINT)
- * б) Выполнить 2-3 SQL операции на ваше усмотрение (например, Insert в 3 таблицы – USER, ROLE, USER_ROLE)
- * между sql операциями установить точку сохранения (SAVEPOINT A),
- * намеренно ввести некорректные данные на последней операции, что бы транзакция откатилась к логической точке SAVEPOINT A
+ * Задание 2
+ * Используя Spy и Mockito создать заглушки для интерфейса JDBC
  */
 
 public class Main {
